41eb8093b941c16a677fcbdff58c5b0f010ddaae
ivanGonzalez0 <ig011100011@gmail.com>
2024-11-22 19:19:30 -0300

    cambios en la clase Joker

--- joker.ts
+++ joker.ts
@@ -1,13 +1,73 @@
+import * as rls from 'readline-sync';
 import { Juego } from "./juego";
 
-export class Joker extends Juego{
+export class Joker extends Juego {
     private combinacion: string;
-    private simbolo: string;
-    private carrete: number;
+    private carrete: string[];
 
-    constructor(combinacion: string, nombre:string, min:number, max: number){
-        super("Joker", 200, 1000)
-        this.combinacion= combinacion;
+    public constructor(combinacion: string) {
+        super("Joker", 200, 3000);
+        this.combinacion = combinacion;
+        this.carrete = ["🚀", "🤡", "🎃", "💎"];
     }
-    
+
+    public jugar(): void {
+        let jugar: string = rls.question("presione 'A' para girar el carrete: ");
+        if (jugar === "A") {
+            console.log("los carretes estan girando");
+        }
+        const resultado = this.carrete[Math.floor(Math.random() * this.carrete.length)];
+        console.log(`Resultado: ${resultado}`);
+        this.obtenerResultado();
+        
+    }
+
+
+    public apuesta(): number {
+            let apuestaUser: number=rls.questionInt("ingrese su apuesta: ")
+            if(apuestaUser >200 && apuestaUser <3000){
+                return apuestaUser;
+        } else {
+            console.log("ingrese nuevamente su apuesta.");
+        }
+        return apuestaUser;
 }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     public jugar(): void {
+//         let jugar: string = rls.question("presione 'A' para girar el carrete: ");
+//         if (jugar === "A") {
+
+//             console.log("los carretes estan girando", this.carrete);
+//         }
+        
+//     }
+// }
+//   private jugar(): void {
+//     console.log(¡Los rodillos están girando!);
+//     const resultado = this.girarRodillos();
+//     const coincidencias = this.calcularCoincidencias(resultado);
+//     console.log(Resultado: ${ resultado.join( | ) });
+//     this.mostrarPremio(coincidencias);
+// }
+
+
