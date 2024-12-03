a820be11cdec89662473d1902f482dc05049227e
Andreafullstack <andreasanjuan21@gmail.com>
2024-12-02 21:13:26 -0300

    cree clase joker

--- joker.ts
+++ joker.ts
@@ -1,27 +1,37 @@
 import * as rls from 'readline-sync';
 import { Juego } from "./juego";
+import * as fs from 'fs';//
 
 export class Joker extends Juego {
-    private simbolos: string[];
-    private carrete: string[];
-    private credito: number;
+    protected simbolos: string[];
+    protected carrete: string[];
+    protected credito: number;
+    protected apuestaActual: number;
+    protected apuestaUser: number=0;
 
-    public constructor() {
-        super("Joker", 200, 3000);
-        this.simbolos= ["🚀", "🤡", "🎃", "🎇", "👑","💰"];
+    public constructor() { 
+        super("Joker");
+        // this.simbolos = ["🚀", "🤡", "🎃", "🎇", "👑", "💰"];
+        this.simbolos = ["💰", "💰", "💰"];
         this.carrete = [];
-        this.credito= 100000;
+        this.credito = 100000;
+        this.apuestaActual = 0;
     }
-    
+
     public apuesta(): number {
-        let apuestaUser: number;
-        while (true) {
-            apuestaUser = rls.questionInt("Ingrese su apuesta ( entre " + this.apuestaMin() + " y " + this.apuestaMax()+ "): ");
-            if (apuestaUser >= this.apuestaMin() && apuestaUser <= this.apuestaMax()) {
-                return apuestaUser;
-            } else {
-                console.log("Ingrese una apuesta válida.");
+        if (this.apuestaActual === 0) {
+            while (true) {
+                this.apuestaUser = rls.questionInt("Ingrese su apuesta entre " + this.apuestaMin() + " y " + this.apuestaMax() + ": ");
+
+                if (this.apuestaUser >= this.apuestaMin() && this.apuestaUser <= this.apuestaMax()) {
+                    this.apuestaActual = this.apuestaUser;
+                    return this.apuestaUser;
+                } else {
+                    console.log("Ingrese una apuesta válida.");
+                }
             }
+        }else {
+            return this.apuestaActual;
         }
     }
 
@@ -30,29 +40,39 @@ export class Joker extends Juego {
         if (jugar === "A") {
             console.log("los carretes estan girando");
         }
-        this.girarCarrete();
-        // JOIN: combina filas para obtener un resultado del array
+        this.girarCarrete(); 
+     
         console.log(`Carrete: ${this.carrete.join(', ')}`);
         if (this.carrete[0] === this.carrete[1] && this.carrete[1] === this.carrete[2]) {
             console.log('¡Ganaste!');
-            this.credito += this.apuesta();
+            this.credito += this.apuestaUser;
         } else {
             console.log('¡Perdiste!');
-            this.credito -= this.apuesta();
+            this.credito -= this.apuestaUser;
         }
         console.log(`Tu crédito actual es: ${this.credito}`);
+        this.apuestaActual = 0; 
     }
 
     public girarCarrete(): void {
         this.carrete = [];
         for (let i = 0; i < 3; i++) {
-          const indice = Math.floor(Math.random() * this.simbolos.length);
-        this.carrete.push(this.simbolos[indice]);
+            const indice = Math.floor(Math.random() * this.simbolos.length);
+            this.carrete.push(this.simbolos[indice]);
+        }
+    }
+
+    public leerInstrucciones(): void {
+        try {
+            const instrucciones = fs.readFileSync('joker.txt', 'utf8');
+            console.log(instrucciones);//copia tpfinal/joker.ts
+        } catch (error) {
+            console.log('Error al leer el archivo de instrucciones');
         }
     }
 
     public mostrarCredito(): void {
-            console.log(`tu credito es: ${this.credito}`);
+        console.log(`Tu credito es: ${this.credito}`);
     }
 
     public apuestaMin(): number {
