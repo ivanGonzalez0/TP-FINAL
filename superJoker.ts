0a5a0003dc3a165b6220c6300c2f5caaf2ed3027
ritaalediaz <142689358+ritaalediaz@users.noreply.github.com>
2024-12-02 21:17:31 -0300

    Update superJoker.ts
    se borraron comentarios

--- superJoker.ts
+++ superJoker.ts
@@ -1,8 +1,8 @@
-import { Joker } from './joker'; //importa la clase joker
-import * as fs from 'fs'; //Importa el módulo del sistema de archivos (fs) para permitir la lectura de archivos.
+import { Joker } from './joker';
+import * as fs from 'fs'; 
 
 
-export class SuperJoker extends Joker { //Define la clase SuperJoker que hereda de Joker.
+export class SuperJoker extends Joker { 
     private bonus: string[];
 
     public constructor() {
@@ -18,18 +18,18 @@ export class SuperJoker extends Joker { //Define la clase SuperJoker que hereda 
     }
     public leerInstrucciones(): void {
         try {
-            const instrucciones = fs.readFileSync('superJoker.txt', 'utf8'); //lee archivo sincrónicamente.
-            console.log(instrucciones);//Si el archivo se lee correctamente, su contenido se muestra en la consola
-        } catch (error) { //Si ocurre un error, se muestra un mensaje de error
+            const instrucciones = fs.readFileSync('superJoker.txt', 'utf8'); 
+            console.log(instrucciones);
+        } catch (error) { 
             console.log('Error al leer el archivo de instrucciones');
         }
     }
 
-    public ganarBonus(): void { //Método que otorga un premio adicional.
+    public ganarBonus(): void {
         console.log("¡FELICIDADES GANASTE EL BONUS!");
-        const multiplicador = this.bonus.length; // Utiliza la cantidad de elementos en el bonus como multiplicador
-        this.credito *= multiplicador; // Multiplica el crédito por el multiplicador
-        console.log(`Tu crédito se ha multiplicado por ${multiplicador} y ahora es: ${this.credito}`);//Muestra el nuevo crédito del usuario en la consola.
+        const multiplicador = this.bonus.length; 
+        this.credito *= multiplicador; 
+        console.log(`Tu crédito se ha multiplicado por ${multiplicador} y ahora es: ${this.credito}`);
     }
     
     public apuestaMin(): number {
